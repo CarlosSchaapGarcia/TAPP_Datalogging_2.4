a4a1119840fe745465b92299653352f8836279aa
zhicheng liang <zhichengliang12@gmail.com>
2026-02-27 01:23:58 +0100

    routes for battery columns for db, still need to confirm other values to  be stored from the battery or nfc if flashed later on

---
+++ backend/routes/battery.js
@@ -0,0 +1,47 @@
+const pool = require('../pool');
+
+async function batteryRoutes(fastify, options) {
+
+    fastify.post('/', {
+        schema: {
+            body: {
+                type: 'object',
+                required: ['device_id', 'chip_number', 'voltage', 'percent'],
+                properties: {
+                    device_id: { type: 'string', maxLength: 100 },
+                    chip_number: { type: 'integer' },
+                    voltage: { type: 'number' },
+                    percent: { type: 'integer', minimum: 0, maximum: 100 },
+                }
+            }
+        }
+    }, async (req, res) => {
+
+        const { device_id, chip_number, voltage, percent } = req.body;
+
+        const result = await pool.query(
+            `INSERT INTO battery_measurements
+            (device_id, chip_number, voltage, percent)
+             VALUES ($1, $2, $3, $4)
+            RETURNING id, created_at`,
+            [device_id, chip_number, voltage, percent]
+        );
+
+        return {
+            id: result.rows[0].id,
+            created_at: result.rows[0].created_at
+        };
+    });
+
+    fastify.get('/', async () => {
+        const result = await pool.query(
+            `SELECT *
+           FROM battery_measurements
+           ORDER BY created_at DESC
+           LIMIT 50`
+        );
+        return results.rows;
+    })
+}
+
+module.exports = batteryRoutes;
