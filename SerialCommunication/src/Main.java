a661064cefe858b28b0e2ee8a8f7b4edd68cbf7f
StefanBryda <stefan.bryda@student.nhlstenden.com>
2026-03-25 13:37:56 +0100

    fixed problem with converting voltage data
    voltage is now stored correctly in db. still requires nfc integration and date/time information

--- SerialCommunication/src/Main.java
+++ SerialCommunication/src/Main.java
@@ -13,12 +13,18 @@ public class Main
         // Set the CommPort to the one connected to arduino
         SerialPort COM_PORT = SerialPort.getCommPort("COM6");
         COM_PORT.setBaudRate(9600);
+
+        // Database credentials, set to correct one
         final String DB_URL = "jdbc:postgresql://localhost:5432/tapp_db";
         final String USERNAME = "user";
         final String PASSWORD = "qwerty";
 
+        char commChar = 0;
+
+        // Connection to database
         Connection db = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
 
+        // If port fails to open
         if (!COM_PORT.openPort()) {
             System.out.println("Failed to open port");
             return;
@@ -27,21 +33,37 @@ public class Main
         System.out.println("Port opened");
 
         InputStream in = COM_PORT.getInputStream();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 
         try {
+            // Runs constantly
             while (true) {
+                StringBuilder voltageStringBuilder = new StringBuilder();
+                double voltage = -1;
+
+                // Reads characters from COM port one by one and stores them as a string
                 while (in.available() > 0) {
                     int data = in.read();
-                    char c = (char) data;
-                    System.out.println(c);
-                    PreparedStatement st = db.prepareStatement("INSERT INTO \"BatteryVoltage\" " +
-                                                                    "(\"chipID\", \"voltage\") VALUES (?, ?)");
-                    st.setString(1, "1111");
-                    st.setDouble(2, c);
-                    st.executeQuery();
-                    st.close();
+                    commChar = (char) data;
+                    voltageStringBuilder.append(commChar);
+                }
 
+                // Converts a string to a double
+                if (!voltageStringBuilder.isEmpty())
+                {
+                    String voltageString = voltageStringBuilder.toString();
+                    voltage = Double.parseDouble(voltageString);
+                }
+
+                // Sends a query to the database
+                if (voltage != -1)
+                {
+                    System.out.println(voltage);
+                    PreparedStatement st = db.prepareStatement("INSERT INTO \"BatteryVoltage\" " +
+                            "(\"chipID\", \"voltage\") VALUES (?, ?)");
+                    st.setString(1, "1111");
+                    st.setDouble(2, voltage);
+                    st.executeUpdate();
+                    st.close();
                 }
             }
         } catch (Exception e) {
